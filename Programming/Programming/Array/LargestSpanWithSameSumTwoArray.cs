baafef9348d090f6b78962d3dac091ed33026308
Dushyant Bhatt <dubhatt@microsoft.com>
2019-05-01 12:35:59 +0530

    Longest Span with same Sum in two Binary arrays

--- Programming/Programming/Array/LargestSpanWithSameSumTwoArray.cs
+++ Programming/Programming/Array/LargestSpanWithSameSumTwoArray.cs
@@ -1,9 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*Longest Span with same Sum in two Binary arrays
+Given two binary arrays arr1[] and arr2[] of same size n. Find length of the longest common span (i, j) where j >= i such that arr1[i] + arr1[i+1] + …. + arr1[j] = arr2[i] + arr2[i+1] + …. + arr2[j].
 
+Expected time complexity is Θ(n).
+
+Examples :
+
+Input: arr1[] = {0, 1, 0, 0, 0, 0};
+       arr2[] = {1, 0, 1, 0, 0, 1};
+Output: 4
+The longest span with same sum is from index 1 to 4.
+
+Input: arr1[] = {0, 1, 0, 1, 1, 1, 1};
+       arr2[] = {1, 1, 1, 1, 1, 0, 1};
+Output: 6
+The longest span with same sum is from index 1 to 6.
+
+Input: arr1[] = {0, 0, 0};
+       arr2[] = {1, 1, 1};
+Output: 0
+
+Input: arr1[] = {0, 0, 1, 0};
+       arr2[] = {1, 1, 1, 1};
+Output: 1*/
 namespace Programming.Array
 {
     public class LargestSpanWithSameSumTwoArray
